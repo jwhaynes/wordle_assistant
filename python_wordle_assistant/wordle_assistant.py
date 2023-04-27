97cb9f19c282ae6fd7639bb9166ebfe94a02e846
jwhaynes <jhaynes2112@gmail.com>
2023-04-27 17:15:27 -0400

    added words to database using python then commented out python

--- python_wordle_assistant/wordle_assistant.py
+++ python_wordle_assistant/wordle_assistant.py
@@ -13,5 +13,20 @@ db_cursor = db_connection.cursor()
 #db_cursor.execute('CREATE TABLE wordle_words(words)') ### commented out because table only needs to be created once ###
 
 # check schema table to see that table exists
-res = db_cursor.execute('SELECT name FROM sqlite_master')
-print(res.fetchone())
+#res = db_cursor.execute('SELECT name FROM sqlite_master')
+#print(res.fetchone())
+
+
+### insert the words of the source text file into the sqlite3 database ###
+words_list = []
+with open('./python_wordle_assistant/wordle-nyt-words-14855.txt','r') as words_file:
+    file_lines = words_file.readlines()
+    for line in file_lines:
+        word = [line.strip()]
+        words_list.append(word)
+
+#db_cursor.executemany('INSERT INTO wordle_words VALUES(?)', words_list)
+#db_connection.commit()
+
+total_in_db = db_cursor.execute('SELECT * FROM wordle_words')
+print(len(total_in_db.fetchall()))
